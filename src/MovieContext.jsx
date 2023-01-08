18b1788ab3962be16fe982cd69dc1e196a5001d3
achintha <achinthathennakoon97@gmail.com>
2023-01-08 16:33:43 +0530

    add movie lsit

--- src/MovieContext.jsx
+++ src/MovieContext.jsx
@@ -1,5 +1,17 @@
-import React from 'react'
+import React, { useState , createContext} from 'react'
+
+
 
 export const MovieProvider = () =>{
+  const [movies,setMovies] = useState([
+    {
+      id: 1,
+      name: "harry potter",
+    },
+    {
+      id: 2,
+      name: "1899",
+    },
+  ]);
   return();
 }
