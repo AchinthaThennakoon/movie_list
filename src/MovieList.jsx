9c591d1f67edd277199b23d29533085d9cbf04bf
achintha <achinthathennakoon97@gmail.com>
2023-01-08 16:31:25 +0530

    remove movie list

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -2,16 +2,6 @@ import React from "react";
 import Movie from "./Movie";
 
 function MovieList() {
-  var movies = [
-    {
-      id: 1,
-      name: "harry potter",
-    },
-    {
-      id: 2,
-      name: "1899",
-    },
-  ];
   return (
     <>
       {movies.map((movie) => (
