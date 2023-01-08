b48a6af05e960b4f4c7bd70f1cf77e15bb3ceaf5
achintha <achinthathennakoon97@gmail.com>
2023-01-08 14:13:44 +0530

    add movie list and pass to movie using props

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Movie from './Movie'
+import React from "react";
+import Movie from "./Movie";
 
 function MovieList() {
+  var movies = [
+    {
+      id: 1,
+      name: "harry potter",
+    },
+    {
+      id: 2,
+      name: "1899",
+    },
+  ];
   return (
     <>
-    <Movie/>
+      {movies.map((movie) => (
+        <Movie name={movie.name} id={movie.id} />
+      ))}
     </>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
